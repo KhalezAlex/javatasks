749d86ed3bb159f644055eb11e4876481543696f
KhalezAlex <klozevitz@yandex.ru>
2023-10-22 16:33:53 +0300

    <feature> [database-connection] - добавлен класс работы с бд- AgentDB.java - добавлен postgresql-42.6.0.jar- jdbc driver - добавлен класс entity- Patient.java

--- src/main/java/ru/spb/reshenie/javatasks/Test_app.java
+++ src/main/java/ru/spb/reshenie/javatasks/Test_app.java
@@ -4,17 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ru.spb.reshenie.javatasks.db.AgentDB;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Test_app extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Test_app.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        AgentDB agentDB = new AgentDB();
+        System.out.println(agentDB.getPersonsByString("нна"));
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/ru/spb/reshenie/javatasks/entity/Patient.java
@@ -0,0 +1,99 @@
+package ru.spb.reshenie.javatasks.entity;
+
+import java.time.LocalDate;
+
+public class Patient {
+    private Integer id;
+    private String fio;
+    private LocalDate birthDate;
+    private Integer sex;
+    private Integer num;
+    private String smo;
+    private String snils;
+    private String policy;
+    private Integer finSource;
+    private Integer age;
+
+    public Integer getFinSource() {
+        return finSource;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public String getSmo() {
+        return smo;
+    }
+
+    public String getSnils() {
+        return snils;
+    }
+
+    public String getPolicy() {
+        return policy;
+    }
+
+    public Integer getFin_source() {
+        return finSource;
+    }
+
+    public Patient() {
+    }
+
+    public Patient(Integer id, String fio, Integer num) {
+        this.id = id;
+        this.fio = fio;
+        this.num = num;
+    }
+
+    public Patient(Integer id, String fio, LocalDate birthDate, Integer sex,
+                   Integer num, String smo, String snils, String policy, Integer finSource) {
+        this.id = id;
+        this.fio = fio;
+        this.birthDate = birthDate;
+        this.sex = sex;
+        this.num = num;
+        this.smo = smo;
+        this.snils = snils;
+        this.policy = policy;
+        this.finSource = finSource;
+        this.age = LocalDate.now().compareTo(birthDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", fio='" + fio + '\'' +
+                ", birthDate=" + birthDate +
+                ", sex=" + sex +
+                ", num=" + num +
+                ", smo='" + smo + '\'' +
+                ", snils='" + snils + '\'' +
+                ", policy='" + policy + '\'' +
+                ", finSource=" + finSource +
+                ", age=" + age +
+                "}\n";
+    }
+}
